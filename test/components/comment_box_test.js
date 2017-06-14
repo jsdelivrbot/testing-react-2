5a5aeaa5b9ba839c9cd1c216645c5baafaf85dee
wongjenn <wongjenn@yahoo.com>
2017-06-14 16:59:41 -0400

    setup and testing of comment box

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -2,18 +2,21 @@ import { renderComponent, expect } from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(CommentBox);
+  });
+
   it('has the correct class', () => {
-    const component = renderComponent(CommentBox);
-    expect(component).to.have.class('CommentBox');
+    expect(component).to.have.class('comment-box');
   });
 
   it('has a text area', () => {
-    const component = renderComponent(CommentBox);
     expect(component.find('textarea')).to.exist;
   });
 
-  it('it has a button', () => {
-    const component = renderComponent(CommentBox);
+  it('has a button', () => {
     expect(component.find('button')).to.exist;
   });
 });
